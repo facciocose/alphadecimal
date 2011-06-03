0e473fabe86abac609effe5ad4bb06b1eae4d26d
Luca Marra <ldm.marra@gmail.com>
2011-06-03 19:46:35 +0200

    invert A-B with a-b

--- lib/alphadecimal.rb
+++ lib/alphadecimal.rb
@@ -15,8 +15,8 @@ module Alphadecimal
       until value == 0
         case mod = value % 62
         when 0..9 then string << (mod + B62_0)
-        when 10..35 then string << (mod - 10 + B62_A)
-        when 36...62 then string << (mod - 36 + B62_a)
+        when 10..35 then string << (mod - 10 + B62_a)
+        when 36...62 then string << (mod - 36 + B62_A)
         end
         value = value / 62
       end
@@ -32,8 +32,8 @@ module Alphadecimal
       key.bytes.each_with_index do |char, i|
          case
          when (B62_0..B62_9).include?(char) then norm = char - B62_0
-         when (B62_A..B62_Z).include?(char) then norm = char - B62_A + 10
-         when (B62_a..B62_z).include?(char) then norm = char - B62_a + 36
+         when (B62_a..B62_z).include?(char) then norm = char - B62_a + 10
+         when (B62_A..B62_Z).include?(char) then norm = char - B62_A + 36
          end
         val = val + (norm * (62 ** i))
       end
